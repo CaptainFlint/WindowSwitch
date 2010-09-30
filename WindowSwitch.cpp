3bb8c416e3c327ae59fe9de664a1c7aaf056e4c4
Konstantin Vlasov <support@flint-inc.ru>
2010-09-30 17:30:41 +0400

    Improved next window calculation.
    1. Independent windows of the same process are now allowed.
    2. Multi-windowed applications are correctly processed.

--- WindowSwitch.cpp
+++ WindowSwitch.cpp
@@ -17,19 +17,30 @@ int WinMainCRTStartup(void)
 			if (current_wnd == NULL)
 				continue;
 
-			DWORD pid, next_pid;
-			GetWindowThreadProcessId(current_wnd, &pid);
+			// Find top-level owner of the current window
+			HWND owner = current_wnd;
+			do 
+			{
+				current_wnd = owner;
+				owner = GetWindow(current_wnd, GW_OWNER);
+			} while ((owner != NULL) && IsWindowVisible(owner));
 
-			HWND next_wnd = current_wnd;
+			// Find next window in Z-stack to switch to
 			do {
-				next_wnd = GetNextWindow(next_wnd, GW_HWNDNEXT);
-				if (next_wnd == NULL)
+				current_wnd = GetNextWindow(current_wnd, GW_HWNDNEXT);
+				if (current_wnd == NULL)
 					break;
-				GetWindowThreadProcessId(next_wnd, &next_pid);
-			} while(!IsWindowVisible(next_wnd) || (next_pid == pid));
+				owner = GetWindow(current_wnd, GW_OWNER);
+				// Skip all windows that:
+				// a) are invisible, or
+				// b) have extended style WS_EX_TOOLWINDOW set, or
+				// c) have existing and visible owner window.
+			} while (!IsWindowVisible(current_wnd) ||
+			         ((GetWindowLongPtr(current_wnd, GWL_EXSTYLE) & WS_EX_TOOLWINDOW) != 0) ||
+			         ((owner != NULL) && IsWindowVisible(owner)));
 
-			if (next_wnd != NULL)
-				SetForegroundWindow(next_wnd);
+			if (current_wnd != NULL)
+				SetForegroundWindow(current_wnd);
         }
     }
 	UnregisterHotKey(NULL, 0);
